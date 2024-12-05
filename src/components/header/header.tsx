ababed5e1a45e11c344a316420cf54dcb7983fdc
Davide <doreglio@red.software.systems>
2024-12-05 15:10:09 +0100

    add left content of Header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,9 +1,25 @@
+import Link from "next/link";
 import style from "./header.module.css";
+import Image from "next/image";
+import { CiLocationOn } from "react-icons/ci";
+
 export default function Header() {
     return (
         <header className={style.header}>
-            <div className={style.header__top}>dsa</div>
-            <div className={style.header__bottom}>sad</div>
+            <div className={style.header__top}>
+                <Link className="hover-border" href="/">
+                    <Image src="/images/amazon_logo.png" alt="logo" width={115} height={58} />
+                </Link>
+                <div className={`${style.header__top__delivery} hover-border`}>
+                    <CiLocationOn className={style.locationIcon} />
+                    <div>
+                        <span className="text-gray-100">Delivering to Roma 00185</span>
+                        <br />
+                        <span className="fw-700">Update locations</span>
+                    </div>
+                </div>
+            </div>
+            <div className={style.header__bottom}>happy :-)</div>
         </header>
     );
 }
