b6402fdffeda68a59908b3905567be6b289c5048
Davide <doreglio@red.software.systems>
2024-12-05 17:21:42 +0100

    c

---
+++ src/app/api/products/route.ts
@@ -0,0 +1,9 @@
+import { Product } from "@/types/product.type";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    return NextResponse.json<Product[]>([
+        { name: "pc", price: 500 },
+        { name: "ipad", price: 230 },
+    ]);
+}
--- src/components/product-list/product-list.tsx
+++ src/components/product-list/product-list.tsx
@@ -1,11 +1,12 @@
 import { Product } from "@/types/product.type";
 
-export default function ProductList() {
-    const products: Product[] = [{ name: "pc", price: 500 }];
+export default async function ProductList() {
+    const res = await fetch("http://localhost:3000/api/products");
 
+    const products = await res.json();
     return (
         <>
-            {products.map((p, _) => (
+            {products.map((p: Product, _) => (
                 <div key={_}>{p.name}</div>
             ))}
         </>
