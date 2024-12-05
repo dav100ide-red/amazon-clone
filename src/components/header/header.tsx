7f5de77300998fd56edaf891baefe3d70bb71080
Davide <doreglio@red.software.systems>
2024-12-05 14:10:18 +0100

    add header top andbottom

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,9 @@
 import style from "./header.module.css";
 export default function Header() {
-    return <header className={style.header}></header>;
+    return (
+        <header className={style.header}>
+            <div className={style.header__top}>dsa</div>
+            <div className={style.header__bottom}>sad</div>
+        </header>
+    );
 }
