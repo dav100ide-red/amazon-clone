cbbf4df3ad87ecf235ed00d35c719993599210bd
Davide <doreglio@red.software.systems>
2024-12-05 17:21:49 +0100

    c

--- src/components/product-list/product-list.tsx
+++ src/components/product-list/product-list.tsx
@@ -3,7 +3,7 @@ import { Product } from "@/types/product.type";
 export default async function ProductList() {
     const res = await fetch("http://localhost:3000/api/products");
 
-    const products = await res.json();
+    const products: Product[] = await res.json();
     return (
         <>
             {products.map((p: Product, _) => (
