84e5c26118386a02d0585c059f4fe1471bcd7ca7
Davide <doreglio@red.software.systems>
2024-12-05 11:43:33 +0100

    helper classes in typography.css

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,10 @@
 import style from "./header.module.css";
 export default function Header() {
-    return <header className={style.header}>header</header>;
+    return (
+        <header className={style.header}>
+            Header
+            <div className="fst-italic fw-400">test pippo</div>
+            <h2 className="fw-500">h2 pippo</h2>
+        </header>
+    );
 }
