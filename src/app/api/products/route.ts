d28d3b62c2c36bafd5b2a0d342e65598657a66bc
Davide <doreglio@red.software.systems>
2025-01-07 12:49:16 +0100

    add products data

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,18 +1,11 @@
 import wait from "@/functions/wait";
 import { Product } from "@/types/product.type";
 import { NextResponse } from "next/server";
+import { products } from "./data";
 
 export async function GET() {
-    const jsonData = NextResponse.json<Product[]>([
-        {
-            id: 1,
-            name: "pc",
-            price: 500,
-            category: "electronic",
-            imgSrc: "https://images.unsplash.com/photo-1560131914-2e469a0e8607?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2OTQyNjF8MHwxfGFsbHx8fHx8fHx8fDE3MzYyNDg5NjF8&ixlib=rb-4.0.3&q=80&w=1080",
-        },
-    ]);
+    const nextResponse = NextResponse.json<Product[]>(products);
 
-    const slowData = await wait(jsonData, 0);
-    return slowData;
+    const slowRes = await wait(nextResponse, 0);
+    return slowRes;
 }
