82e4b28a9855184aa20982091b4ba2fb46573167
Davide <doreglio@red.software.systems>
2025-01-07 11:21:32 +0100

    fix <Image/> style in product card

--- src/components/product-card/product-card.tsx
+++ src/components/product-card/product-card.tsx
@@ -1,17 +1,22 @@
 import { Product } from "@/types/product.type";
 import style from "./product-card.module.css";
+import sliderImg_2 from "@/assets/images/slider/sliderImg_2.jpg";
+import Image from "next/image";
 
 type ProductCardProps = {
     product: Product;
 };
 
 const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
-    const { name, price, category } = product;
+    const { name, price } = product;
 
     return (
         <div className={style.productCard}>
-            <div className={style.productImage}></div>
             <h2 className={style.productTitle}>{name}</h2>
+            <div className={style.productImg}>
+                <Image src={sliderImg_2} alt="sliderImg" fill style={{ objectFit: "cover" }} />
+            </div>
+
             <div className={style.productPrice}>€{price}</div>
             <div className={style.productIntro}>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.
